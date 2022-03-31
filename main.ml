cdae8ead30c6c5ff892773045e7b07e541d14caa
Alessandro <alessandro@mail.fr>
2022-03-31 23:12:05 +0200

    Correction indentation

--- main.ml
+++ main.ml
@@ -1,10 +1,10 @@
 (**********************************************
 
-       Projet Programmation Fonctionnelle
+	   Projet Programmation Fonctionnelle
 
-                GHITA Alessandro
-                GELINAUD Clement
-                BENMOUSSATI Souhail
+				GHITA Alessandro
+				GELINAUD Clement
+				BENMOUSSATI Souhail
 
 **********************************************)
 
@@ -15,35 +15,35 @@
 open List
 
 type t_typePrimitive =
-	| Entier
-  | Bool
-  | Char
+    | Entier
+    | Bool
+    | Char
 ;;
 
 type t_type =
 	| Type of t_typePrimitive
-  | Fonction of t_type * t_type
-  | Tuple of t_type * t_type
+    | Fonction of t_type * t_type
+    | Tuple of t_type * t_type
 ;;
 
 (* Constantes et opérations *)
-type primitive = 
-  | Un
-  | Deux
-  | Trois
-  | Quatre
-  | Cinq
-  | A
-  | B
-  | C
-  | D
-  | Vrai
-  | Faux
-  | Plus
-  | Moins
-  | Inferieur
-  | Superieur
-  | Condition
+type primitive =
+    | Un
+    | Deux
+    | Trois
+    | Quatre
+    | Cinq
+    | A
+    | B
+    | C
+    | D
+    | Vrai
+    | Faux
+    | Plus
+    | Moins
+    | Inferieur
+    | Superieur
+    | Condition
 ;;
 
 (* Défini le nom d'une variable ou d'une fonction *)
@@ -51,68 +51,68 @@ type name = string;;
 
 
 type t_expr =
-  | Var of name
-  | Constante of primitive
-  | Fonction of name * t_type * t_expr
-  | Application of t_expr * t_expr
-  | Tuple of t_expr * t_expr
-  | Let of name * t_type * t_expr * t_expr
+    | Var of name
+    | Constante of primitive
+    | Fonction of name * t_type * t_expr
+    | Application of t_expr * t_expr
+    | Tuple of t_expr * t_expr
+    | Let of name * t_type * t_expr * t_expr
 ;;
 
 
 (* QUESTION 2 : *)
 
 let t_typePrimitive_print monType =
-	match monType with
-	| Entier -> "Entier"
-  | Bool -> "Bool"
-  | Char -> "Char"
+    match monType with
+    | Entier -> "Entier"
+    | Bool -> "Bool"
+    | Char -> "Char"
 ;;
 
 let primitive_print primitive =
 	match primitive with
 	| Un -> "Un"
-  | Deux -> "Deux"
-  | Trois -> "Trois"
-  | Quatre -> "Quatre"
-  | Cinq -> "Cinq"
-  | A -> "A"
-  | B -> "B"
-  | C -> "C"
-  | D -> "D"
-  | Vrai -> "Vrai"
-  | Faux -> "Faux"
-  | Plus -> "Plus"
-  | Moins -> "Moins"
-  | Inferieur -> "Inferieur"
-  | Superieur -> "Superieur"
-  | Condition -> "Condition"
+    | Deux -> "Deux"
+    | Trois -> "Trois"
+    | Quatre -> "Quatre"
+    | Cinq -> "Cinq"
+    | A -> "A"
+    | B -> "B"
+    | C -> "C"
+    | D -> "D"
+    | Vrai -> "Vrai"
+    | Faux -> "Faux"
+    | Plus -> "Plus"
+    | Moins -> "Moins"
+    | Inferieur -> "Inferieur"
+    | Superieur -> "Superieur"
+    | Condition -> "Condition"
 ;;
-          
+
 let rec t_type_print monType =
   match monType with
   | Type type1 -> t_typePrimitive_print type1
   | Fonction(type1, type2) -> (
-  	t_type_print type1 ^ " -> " ^ t_type_print type2
-  	)
+	t_type_print type1 ^ " -> " ^ t_type_print type2
+	)
   | Tuple(type1, type2) ->
-  	
-  	(* L'ordre du filtrage est très important afin de ne pas rentré
-  			directement dans le cas de base *)
-  	match type1, type2 with
-     (* Double tuple => (type * type) * (type * type) *)
-     | Tuple(_, _), Tuple(_, _) ->  (
-     		"(" ^ t_type_print type1 ^ ") * (" ^ t_type_print type2 ^ ")"
-     	)
-     
-     (* Le tuple est uniquement à gauche => (type * type) * type			*)
-     | Tuple(_, _), _ ->  "(" ^ t_type_print type1 ^ ") * " ^ t_type_print type2
-     
-     (* Le tuple est uniquement à droite => type * (type * type)			*)
-     | _, Tuple(_, _) ->  t_type_print type1 ^ " * (" ^ t_type_print type2 ^ ")"
-     	
-     (* Cas de base, un tuple simple => type * type										*)
-     | _, _ ->  t_type_print type1 ^ " * " ^ t_type_print type2
+
+	(* L'ordre du filtrage est très important afin de ne pas rentré
+			directement dans le cas de base *)
+	match type1, type2 with
+	 (* Double tuple => (type * type) * (type * type) *)
+	 | Tuple(_, _), Tuple(_, _) ->  (
+			"(" ^ t_type_print type1 ^ ") * (" ^ t_type_print type2 ^ ")"
+		)
+
+	 (* Le tuple est uniquement à gauche => (type * type) * type			*)
+	 | Tuple(_, _), _ ->  "(" ^ t_type_print type1 ^ ") * " ^ t_type_print type2
+
+	 (* Le tuple est uniquement à droite => type * (type * type)			*)
+	 | _, Tuple(_, _) ->  t_type_print type1 ^ " * (" ^ t_type_print type2 ^ ")"
+
+	 (* Cas de base, un tuple simple => type * type										*)
+	 | _, _ ->  t_type_print type1 ^ " * " ^ t_type_print type2
 ;;
 
 
@@ -121,12 +121,12 @@ let rec t_expr_print expression =
   | Var var -> var
   | Constante constante -> primitive_print constante
   | Fonction(name, type1, expression) -> (
-  		"fun " ^ name ^ " : " ^ t_type_print type1 ^ " -> " ^ t_expr_print expression
-  	)
+		"fun " ^ name ^ " : " ^ t_type_print type1 ^ " -> " ^ t_expr_print expression
+	)
   | Application(expression1, expression2) -> t_expr_print expression1 ^ " " ^ t_expr_print expression2
   | Tuple(expression1, expression2) -> "(" ^ t_expr_print expression1 ^ ", " ^ t_expr_print expression2 ^ ")"
   | Let(nom, type1, expression1, expression2) ->
-     "let " ^ nom ^ " : " ^ t_type_print type1 ^ " = " ^ t_expr_print expression1 ^ " in " ^ t_expr_print expression2
+	 "let " ^ nom ^ " : " ^ t_type_print type1 ^ " = " ^ t_expr_print expression1 ^ " in " ^ t_expr_print expression2
 ;;
 
 let expression = Let("decrement", Fonction(Type Entier, Type Entier),
@@ -143,8 +143,8 @@ type environement = (t_expr * t_type) list;;
 
 (* On initialise notre environnement *)
 let environement_construction =
-    (Constante Un, Type Entier)
-    :: (Type Entier * Type Entier)
+	(Constante Un, Type Entier)
+	:: (Type Entier * Type Entier)
   ::(Constante Deux, Type Entier)
   ::(Constante Trois, Type Entier)
   ::(Constante Quatre, Type Entier)
@@ -168,9 +168,9 @@ let rec environnement_print environnement =
   match environnement with
   | [] -> ()
   | tete::reste ->
-     print_string (t_expr_print (fst tete) ^ " : " ^ (t_type_print (snd tete)));
-     print_newline();
-     environnement_print reste;                                                    
+	 print_string (t_expr_print (fst tete) ^ " : " ^ (t_type_print (snd tete)));
+	 print_newline();
+	 environnement_print reste;
 ;;
 
 (*** Question 4 ***)
@@ -178,7 +178,7 @@ let rec environnement_print environnement =
 (* Nous avons rencontrés des difficultés pour coder cette fonction,
 		en particulier lors des essaies de la question 5,
 		on obtiens : Exception: Failure "type [Entier * Entier] inexistant".
-		
+
 		Malgré nos efforts, nous n'avons pas su résoudre le(s) problème(s)
 *)
 let rec verif_type environnement expression =
@@ -186,23 +186,23 @@ let rec verif_type environnement expression =
   | Var valeur -> List.assoc (Var valeur) environnement
   | Constante cte  -> List.assoc (Constante cte) environnement
   | Fonction(v_local, t_local, exp_local) -> let env_local = (Var v_local, t_local)::environnement in
-                                            Fonction(t_local, verif_type env_local exp_local)
+											Fonction(t_local, verif_type env_local exp_local)
   | Application(exp1, exp2) ->
-     begin
-       match verif_type environnement exp1, verif_type environnement exp2 with
-       |Fonction(t_local, tbis_local), myType ->
-         if t_local == myType then tbis_local
-         else failwith ("type [" ^ t_type_print t_local ^ "] inexistant")
-       |_ -> failwith ("erreur expression -> pas une fonction")
-     end
+	 begin
+	   match verif_type environnement exp1, verif_type environnement exp2 with
+	   |Fonction(t_local, tbis_local), myType ->
+		 if t_local == myType then tbis_local
+		 else failwith ("type [" ^ t_type_print t_local ^ "] inexistant")
+	   |_ -> failwith ("erreur expression -> pas une fonction")
+	 end
 
   |Tuple (exp1, exp2) -> Tuple(verif_type environnement exp1, verif_type environnement exp2)
   |Let (v_local, t_local, exp1, exp2) ->
-    if t_local == verif_type environnement exp1
-    then
-      let env_local = (Var v_local, t_local)::environnement in
-      verif_type env_local exp2
-    else failwith ("Erreur type -->" ^ t_type_print t_local)
+	if t_local == verif_type environnement exp1
+	then
+	  let env_local = (Var v_local, t_local)::environnement in
+	  verif_type env_local exp2
+	else failwith ("Erreur type -->" ^ t_type_print t_local)
 ;;
 
 
@@ -213,9 +213,9 @@ let rec verif_type environnement expression =
 (* Pour cette question, nous allons formatter
 		l'expression à l'aide de sous-expressions
 		afin de faciliter la lecture
-		
+
 		On rappelle qu'on veut obtenir l'expression :
-		
+
 		fun x : char -> (let succ : int -> int = fun x : int -> + (x, 1) in (succ 1, x))
 *)
 
